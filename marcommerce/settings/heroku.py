90231ec410703bea193b82d70ddaabf6e4e3bbc4
Pedro da Conceicao <pedrocardo007@hotmail.com>
2022-04-24 19:52:32 -0300

    Setima alteração para rodar o heroku
    Tive que setar o url sem o https inicial

--- marcommerce/settings/heroku.py
+++ marcommerce/settings/heroku.py
@@ -4,12 +4,12 @@ from marcommerce.settings.base import *
 
 env = environ.Env()
 
-# DEBUG = env.bool("DEBUG", False)
+DEBUG = env.bool("DEBUG", False)
 
 SECRET_KEY = env("SECRET_KEY")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
-# DATABASES = {
-#     "default": env.db(),
-# }
+DATABASES = {
+    "default": env.db(),
+}
