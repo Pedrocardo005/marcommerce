7f5b51a08b561a1a2948ae08c896d4789330e56f
Pedro da Conceicao <pedrocardo007@hotmail.com>
2022-05-05 07:21:42 -0300

    Add models de categoria e produto #11 e #10

--- loja/models.py
+++ loja/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    
+    nome = models.CharField(max_length=255)
+    icone = models.CharField(max_length=255)
+
+
+class Produto(models.Model):
+
+    nome = models.CharField(max_length=255)
+    preco = models.FloatField(blank=True, null=True, default=0)
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.CASCADE
+    )
