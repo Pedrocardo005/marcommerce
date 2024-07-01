2b0dd266d7726c50e0f2ab68c85aad7b4acec3ac
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-07-01 07:55:56 -0300

    :wastebasket: :remove #57 Remove arquivos do chat

--- chatapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-
-def chat_page(request, *args, **kwargs):
-    if not request.user.is_authenticated:
-        return redirect('login-user')
-    context = {}
-    return render(request, 'chat/ChatPage.html', context)
