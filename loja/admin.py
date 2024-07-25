69c9fa430bfd0fe449bdc6eb8791665e3c5b5da9
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-07-24 21:04:55 -0300

    :sparkles: feat: SubCategoria na area administrativa

--- loja/admin.py
+++ loja/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from parler.admin import TranslatableAdmin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Categoria, Produto, Endereco, ImagemProduto, CustomUser
+from .models import Categoria, Produto, Endereco, ImagemProduto, CustomUser, SubCategoria
 # Register your models here.
 
 
@@ -25,3 +25,4 @@ admin.site.register(Produto, TranslatableAdmin)
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Endereco)
 admin.site.register(ImagemProduto)
+admin.site.register(SubCategoria)
