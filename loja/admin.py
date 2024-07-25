72a4e4783709a91912b5031f2b2a33f23d907b0d
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-07-24 21:02:54 -0300

    :broom: cleanup: Remove importacao nao usada
    By: Pedro da Conceicao

--- loja/admin.py
+++ loja/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from parler.admin import TranslatableAdmin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
 
 from .models import Categoria, Produto, Endereco, ImagemProduto, CustomUser
 # Register your models here.
