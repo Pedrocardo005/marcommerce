d445bd37101f8b7071b6c239c61e98ce9e793423
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-08-01 23:23:08 -0300

    :goal_net: feat: Adiciona tratamento de erro na view de login
    By: Pedro da Conceicao
    
    Refs #68

--- loja/views_loja/usuario.py
+++ loja/views_loja/usuario.py
@@ -4,11 +4,14 @@ from django.shortcuts import redirect, render
 
 
 def login_user(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return redirect('index')
-
-    return render(request, 'login.html', { 'error': True })
+    try:
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        
+        return render(request, 'login.html', { 'error': 'Usuário não encontrado' })
+    except Exception:
+        return render(request, 'login.html', { 'error': 'Ocorreu um erro interno no servidor' })
