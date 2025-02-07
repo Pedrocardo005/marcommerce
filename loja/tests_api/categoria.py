075c22f6b1b8a6aea62d8f0ecc735381d37021db
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-02-07 10:26:05 -0300

    :white_check_mark: test: Cria tests cat subcat
     Criado testes para testar o endpoint que tras as categorias junto com as suas subcategorias.
    
    By: Pedro da Conceicao
    Refs: #84

---
+++ loja/tests_api/categoria.py
@@ -0,0 +1,25 @@
+import json
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class CategoriaTestCase(APITestCase):
+    def test_get_all_cat_subcat(self):
+        url = reverse('loja.cat-subcat')
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(len(response), 13)
+
+        response = self.client.get(url, {'lang': 'en'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(len(response), 0)
+
+        response = self.client.get(url, {'lang': 'es'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(len(response), 0)
