18a0db5d396bd497c4bb6d3d7946a2f729ad07c9
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-02-13 10:03:58 -0300

    :memo: docs: Cria documentacao trazer anuncio
     Atualiza o serializer de anuncio.
    
    By: Pedro da Conceicao
    Refs: #92

--- loja/serializers.py
+++ loja/serializers.py
@@ -6,7 +6,7 @@ from loja.models import Anuncio, Categoria, SubCategoria
 class SubCategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubCategoria
-        fields = ['name']
+        fields = ["name"]
 
 
 class CategoriaSerializer(serializers.ModelSerializer):
@@ -14,13 +14,13 @@ class CategoriaSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Categoria
-        fields = ['nome', 'subcategorias']
+        fields = ["nome", "subcategorias"]
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
 
-        if not self.context.get('show_subcategorias', True):
-            representation.pop('subcategorias', None)
+        if not self.context.get("show_subcategorias", True):
+            representation.pop("subcategorias", None)
 
         return representation
 
@@ -31,45 +31,67 @@ class SearchAnuncioSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Anuncio
-        fields = ['id', 'titulo', 'preco', 'descricao', 'condicao']
+        fields = ["id", "titulo", "preco", "descricao", "condicao"]
 
 
 class GetAnuncioSerializer(serializers.ModelSerializer):
 
     preco = serializers.FloatField()
 
-    id_anunciante = serializers.IntegerField(
-        source='usuario.id',
-        read_only=True
-    )
+    id_anunciante = serializers.IntegerField(source="usuario.id", read_only=True)
 
-    email_anunciante = serializers.CharField(
-        source='usuario.email',
-        read_only=True
-    )
+    email_anunciante = serializers.CharField(source="usuario.email", read_only=True)
 
     class Meta:
         model = Anuncio
-        fields = ['titulo', 'preco', 'views', 'data_publicacao',
-                  'id_anunciante', 'email_anunciante']
+        fields = [
+            "titulo",
+            "preco",
+            "views",
+            "data_publicacao",
+            "id_anunciante",
+            "email_anunciante",
+        ]
 
 
 class UpdateAnuncioSerializer(serializers.ModelSerializer):
 
     preco = serializers.FloatField(read_only=True)
 
-    sub_categoria_id = serializers.IntegerField(
-        source='sub_categoria.id'
+    sub_categoria_id = serializers.IntegerField(source="sub_categoria.id")
+
+    categoria_name = serializers.CharField(
+        source="sub_categoria.categoria.nome", read_only=True
+    )
+
+    sub_categoria_name = serializers.CharField(
+        source="sub_categoria.name", read_only=True
     )
 
     class Meta:
         model = Anuncio
-        fields = ['vendendo', 'titulo', 'descricao', 'tipo_oferta',
-                  'preco', 'condicao', 'envio', 'pagamento_paypal',
-                  'codigo_postal', 'cidade', 'rua', 'numero',
-                  'provedor', 'telefone', 'sub_categoria_id']
+        fields = [
+            "vendendo",
+            "titulo",
+            "descricao",
+            "tipo_oferta",
+            "preco",
+            "condicao",
+            "envio",
+            "custo_envio",
+            "pagamento_paypal",
+            "codigo_postal",
+            "cidade",
+            "rua",
+            "numero",
+            "provedor",
+            "telefone",
+            "sub_categoria_id",
+            "categoria_name",
+            "sub_categoria_name",
+        ]
 
     def update(self, instance, validated_data):
-        sub_categoria = validated_data.pop('sub_categoria')
-        instance.sub_categoria_id = sub_categoria.pop('id')
+        sub_categoria = validated_data.pop("sub_categoria")
+        instance.sub_categoria_id = sub_categoria.pop("id")
         return super().update(instance, validated_data)
