00cc5516b724139a57b6feabad149692e2e6b2c9
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-07-23 20:40:30 -0300

    :wrench: chore: Add static files gitignore
    By: Pedro

--- marcommerce/settings/base.py
+++ marcommerce/settings/base.py
@@ -142,7 +142,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 LOCALE_PATHS = [
