17af12086fc705594110b3ede822fe45feea7121
Pedro da Conceicao <pedrocardo007@hotmail.com>
2026-03-28 17:37:28 -0300

    :sparkles: feat: Save photos in differents environments

--- marcommerce/settings/devp.py
+++ marcommerce/settings/devp.py
@@ -7,3 +7,7 @@ from marcommerce.settings.base import *
 # Override base.py settings here
 
 DEBUG = True
+
+DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
